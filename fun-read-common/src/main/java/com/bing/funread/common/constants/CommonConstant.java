356cba5303a44664a276124d94fb9eb31c63e774
zhangfusheng <zhangfusheng@finupgroup.com>
2018-03-09 11:38:09 +0800

    token生成方法

---
+++ fun-read-common/src/main/java/com/bing/funread/common/constants/CommonConstant.java
@@ -0,0 +1,16 @@
+package com.bing.funread.common.constants;
+
+/**
+ * Description:通用常量类
+ * Author: zhangfusheng
+ * Date: 2018/3/9 上午11:24
+ */
+public class CommonConstant {
+
+    /**
+     * 生成token参数：用户ID
+     */
+    public static final String TOKEN_USER_ID = "userId";
+
+    public static final String TOKEN_UNION_ID = "unionId";
+}
--- fun-read-common/src/main/java/com/bing/funread/common/utils/TokenUtil.java
+++ fun-read-common/src/main/java/com/bing/funread/common/utils/TokenUtil.java
@@ -1,6 +1,19 @@
 package com.bing.funread.common.utils;
 
+import com.bing.funread.common.constants.CommonConstant;
 import com.bing.funread.common.domain.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
 
 /**
  * Description:token处理工具类
@@ -9,8 +22,37 @@ import com.bing.funread.common.domain.User;
  */
 public class TokenUtil {
 
-    public static String createToken(User user) {
+    private static final Logger logger = LoggerFactory.getLogger(TokenUtil.class);
 
-        return user.getUnionId();
+    private static final String SECRET = "xiaobing-funread";
+
+    public static String createToken(User user) {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        Key signingKey = new SecretKeySpec(DatatypeConverter.parseBase64Binary(SECRET),
+                signatureAlgorithm.getJcaName());
+        JwtBuilder builder = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .claim(CommonConstant.TOKEN_USER_ID, user.getId())
+                .claim(CommonConstant.TOKEN_UNION_ID, user.getUnionId())
+                .setSubject(user.getUnionId())
+                .setIssuer("fun-read-server")
+                .setIssuedAt(new Date()) // 设置签发时间
+//                .setExpiration(expiration) // 设置过期时间
+                .signWith(signatureAlgorithm, signingKey);
+        return builder.compact();
+    }
+
+    public static Claims parser(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET))
+                    .parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            logger.error("TokenUtil-parser token过期 ：{}.", e);
+            throw e;
+        } catch(Exception e) {
+            logger.error("TokenUtil-parser Invalid token,原因：{}.", e);
+            return null;
+        }
     }
 }
