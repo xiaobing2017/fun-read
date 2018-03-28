65419283bf705c341095e6b3bab03b3771fb2e83
zhangfusheng <zhangfusheng@finupgroup.com>
2018-03-28 10:52:59 +0800

    url加密注释调

--- fun-read-common/src/main/java/com/bing/funread/common/aop/EncryptionResponseBodyAdvice.java
+++ fun-read-common/src/main/java/com/bing/funread/common/aop/EncryptionResponseBodyAdvice.java
@@ -1,38 +1,38 @@
-package com.bing.funread.common.aop;
-
-import com.bing.funread.common.utils.EncryptionAnnotationUtil;
-import com.bing.funread.response.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.MethodParameter;
-import org.springframework.http.MediaType;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-
-/**
- * url加密处理
- */
-@ControllerAdvice(basePackages = "com.bing.funread.server.controller")
-public class EncryptionResponseBodyAdvice implements ResponseBodyAdvice<Result> {
-	
-	private static final Logger logger = LoggerFactory.getLogger(EncryptionResponseBodyAdvice.class);
-
-	@Override
-	public boolean supports(MethodParameter returnType, Class converterType) {
-		return true;
-	}
-
-	@Override
-	public Result beforeBodyWrite(Result body, MethodParameter returnType, MediaType selectedContentType,
-			Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-		try {
-			EncryptionAnnotationUtil.encrypt(body.getValue());
-			return body;
-		} catch (Exception e) {
-			logger.error("url加密处理失败:{}", e);
-			return body;
-		}
-	}
-}
+//package com.bing.funread.common.aop;
+//
+//import com.bing.funread.common.utils.EncryptionAnnotationUtil;
+//import com.bing.funread.response.Result;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.core.MethodParameter;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpResponse;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+//
+///**
+// * url加密处理
+// */
+//@ControllerAdvice(basePackages = "com.bing.funread.server.controller")
+//public class EncryptionResponseBodyAdvice implements ResponseBodyAdvice<Result> {
+//
+//	private static final Logger logger = LoggerFactory.getLogger(EncryptionResponseBodyAdvice.class);
+//
+//	@Override
+//	public boolean supports(MethodParameter returnType, Class converterType) {
+//		return true;
+//	}
+//
+//	@Override
+//	public Result beforeBodyWrite(Result body, MethodParameter returnType, MediaType selectedContentType,
+//			Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+//		try {
+//			EncryptionAnnotationUtil.encrypt(body.getValue());
+//			return body;
+//		} catch (Exception e) {
+//			logger.error("url加密处理失败:{}", e);
+//			return body;
+//		}
+//	}
+//}
--- fun-read-server/src/main/java/com/bing/funread/server/controller/StaticResourceController.java
+++ fun-read-server/src/main/java/com/bing/funread/server/controller/StaticResourceController.java
@@ -1,38 +1,38 @@
-package com.bing.funread.server.controller;
-
-import com.bing.funread.common.utils.EncryptionUtil;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.apache.commons.lang3.StringUtils;
-import org.hibernate.validator.constraints.NotBlank;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-/**
- * Description:静态资源文件重定向
- * Author: zhangfusheng
- * Date: 2018/3/24 下午2:35
- */
-@Controller
-@Api(value = "/static", description = "练习题接口")
-@RequestMapping("/static")
-public class StaticResourceController {
-
-    private static final String FORWARD = "forward:/";
-
-    @RequestMapping(value = "/{filepath}", method = RequestMethod.GET)
-    @ApiOperation(value = "访问静态资源文件", httpMethod = "GET", notes = "访问静态资源文件")
-    public String accessFile(@ApiParam(required = true, name = "filepath", value = "文件路径")
-                               @NotBlank(message="文件路径不能为空") @PathVariable String filepath,
-                               RedirectAttributes attr) {
-        String realPath = EncryptionUtil.decrypt(filepath);
-        if (StringUtils.isEmpty(realPath)) {
-            return null;
-        }
-        return FORWARD + realPath;
-    }
-}
+//package com.bing.funread.server.controller;
+//
+//import com.bing.funread.common.utils.EncryptionUtil;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.ApiParam;
+//import org.apache.commons.lang3.StringUtils;
+//import org.hibernate.validator.constraints.NotBlank;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+///**
+// * Description:静态资源文件重定向
+// * Author: zhangfusheng
+// * Date: 2018/3/24 下午2:35
+// */
+//@Controller
+//@Api(value = "/static", description = "练习题接口")
+//@RequestMapping("/static")
+//public class StaticResourceController {
+//
+//    private static final String FORWARD = "forward:/";
+//
+//    @RequestMapping(value = "/{filepath}", method = RequestMethod.GET)
+//    @ApiOperation(value = "访问静态资源文件", httpMethod = "GET", notes = "访问静态资源文件")
+//    public String accessFile(@ApiParam(required = true, name = "filepath", value = "文件路径")
+//                               @NotBlank(message="文件路径不能为空") @PathVariable String filepath,
+//                               RedirectAttributes attr) {
+//        String realPath = EncryptionUtil.decrypt(filepath);
+//        if (StringUtils.isEmpty(realPath)) {
+//            return null;
+//        }
+//        return FORWARD + realPath;
+//    }
+//}
